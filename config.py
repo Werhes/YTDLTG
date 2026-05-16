3f429335cc0cf03e5a0962d3a7526b09aa240f03
Werhes <a65768795@gmail.com>
2026-05-16 16:09:13 +0300

    Pulic api keys

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    API_ID = int(os.getenv("API_ID", ))
-    API_HASH = os.getenv("API_HASH", '')
+    API_ID = int(os.getenv("API_ID", 2040))
+    API_HASH = os.getenv("API_HASH", 'b18441a1ff607e10a989891a5462e627')
     BOT_TOKEN = os.getenv("BOT_TOKEN", '8699675744:AAEtrLqY_vN4gSLXIw-qVOI6AAPpXtO2pGw')

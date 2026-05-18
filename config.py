ece36f02cfc5f9fb2465f2e5a1ab51252458cf3a
Werhes <a65768795@gmail.com>
2026-05-18 22:10:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # config.py
-BOT_TOKEN = "8699675744:AAEtrLqY_vN4gSLXIw-qVOI6AAPpXtO2pGw"  # Вставь сюда свой токен
+BOT_TOKEN = "ВАШ_ТОКЕН"  # Вставь сюда свой токен
 DOWNLOAD_DIR = "downloads"             # Папка для временных файлов
 MAX_SIZE_MB = 50                       # Лимит Telegram API
